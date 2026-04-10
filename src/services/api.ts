9979677ed808cf38a9e6780e046606de4b5aebed
BanjoInertia <nozembro@gmail.com>
2026-04-10 01:06:43 -0300

    feat: add production API URL (Render deploy)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import Constants from 'expo-constants';
 
+const PRODUCTION_URL = 'https://cineexplorer-api-c18n.onrender.com';
+
 const debuggerHost = Constants.expoConfig?.hostUri;
 const localhost = debuggerHost?.split(':')[0];
 
 const api = axios.create({
-    baseURL: `http://${localhost}:8000`,
+    baseURL: __DEV__ ? `http://${localhost}:8000` : PRODUCTION_URL,
 });
 
-export default api;
+export default api;
